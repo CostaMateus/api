1e9df6d0294136a03ede5260eb5a024e2e85e631
MateusCosta <costa.mack95@gmail.com>
2019-08-22 13:19:14 -0300

    Review completed

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends Resource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'costumer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star
